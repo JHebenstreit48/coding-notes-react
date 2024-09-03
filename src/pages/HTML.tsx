8fb30882a36572b00e6a8647d107acc079465ddf
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-09-03 12:00:47 -0400

    fix html notes and css

--- src/pages/HTML.tsx
+++ src/pages/HTML.tsx
@@ -2,7 +2,7 @@ import HTMLCodeExamples from "../components/HTMLCodeExamples";
 import Header from "../components/Header";
 import '../CSS/PageCSS.css'
 import DynamicNotes from "../components/DynamicNotes";
-import { HTMLNotes } from "../components/HTMLNotes";
+import items from "../components/HTMLNotes";
 
 export default function HTML() {
 
@@ -10,7 +10,7 @@ export default function HTML() {
 
         <>
         <Header text="HTML"/>
-        <DynamicNotes items={ HTMLNotes} />
+        <DynamicNotes items={items}/>
         <h1>Code Examples</h1>
         <HTMLCodeExamples />
         </>
