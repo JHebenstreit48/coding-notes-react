8c9c8ffac2cf3020a0dc73c9ee8a44db43b4b2d2
Justin <j.hebenstreit25@gmail.com>
2024-09-06 12:15:20 -0500

    trying to fix Netlify issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 const rootElement = document.getElementById('root');
+import './CSS/Page.css';
 
 if (rootElement) {
 
