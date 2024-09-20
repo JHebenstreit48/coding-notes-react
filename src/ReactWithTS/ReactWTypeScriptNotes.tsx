b9c2824c88e606573db324bacc950b1ea87e8339
Justin <j.hebenstreit25@gmail.com>
2024-09-19 23:39:32 -0500

    added more code to ReactWTypescript

--- src/ReactWithTS/ReactWTypeScriptNotes.tsx
+++ src/ReactWithTS/ReactWTypeScriptNotes.tsx
@@ -3,6 +3,44 @@ import { ListItem } from '../Components/Types';
 const items: ListItem[] = [
   {
     content: [
+      { text: 'TypeScript', emphasis: false },
+      { text: 'generics', emphasis: true },
+      { text: 'in React, enable you to create', emphasis: false },
+      { text: 'reusable components', emphasis: true },
+    ],
+  },
+  {
+    content: [
+      { text: 'Generics', emphasis: true },
+      { text: 'allow you to define', emphasis: false },
+      { text: 'components', emphasis: true },
+      { text: 'that are', emphasis: false },
+      { text: 'flexible', emphasis: true },
+      { text: 'enough to work with various', emphasis: false },
+      { text: 'data types,', emphasis: true },
+      { text: 'while still maintaining', emphasis: false },
+      { text: 'type safety.', emphasis: true },
+    ],
+  },
+  {
+    content: [
+      { text: 'The', emphasis: false },
+      { text: 'useContext', emphasis: true },
+      { text: 'hook, allows you to make data', emphasis: false },
+      { text: 'accessible directly', emphasis: true },
+      { text: 'where it is needed in your application', emphasis: false },
+    ],
+  },
+  {
+    content: [
+      { text: '', emphasis: true },
+      { text: '', emphasis: false },
+      { text: '', emphasis: true },
+      { text: '', emphasis: false },
+    ],
+  },
+  {
+    content: [
       { text: 'When you use React ', emphasis: false },
       { text: 'with TypeScript', emphasis: true },
       { text: ' it is best practice to declare our types in a ', emphasis: false },
