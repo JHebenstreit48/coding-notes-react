5428d5221670c06642667fc0256a535b6be802c1
Justin <j.hebenstreit25@gmail.com>
2024-09-24 17:11:29 -0500

    fixed route error

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -76,7 +76,7 @@ export const router = createBrowserRouter([
                 element: <FullStackReact />,
             },
             {
-                path: '/nosql',
+                path: '/mongodb',
                 element: <MongoDB />,
             }
         ],
